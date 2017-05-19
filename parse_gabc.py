380da1e629168e97604371c4ffa6c09584f1a067
benstox <benstox@gmail.com>
2017-05-19 17:23:07 +0100

    Some work on parsing the .gabc files.

--- parse_gabc.py
+++ parse_gabc.py
@@ -6,6 +6,30 @@ import re
 GABC_DIR = "gabc"
 MELODY_DIR = "melody"
 
+NOTE_VALUES = {
+    "c": 0,  # do
+    "d": 2,
+    "w": 3,  # mi bemol
+    "e": 4,
+    "f": 5,
+    "g": 7,
+    "h": 9,
+    "!": 10,  # si bemol
+    "i": 11,
+    "j": 12,  # do
+    "k": 14,  # re
+    "l": 16,  # mi
+    "m": 17,  # fa
+    "n": 19,  # sol
+}
+INVERSE_NOTE_VALUES = {v: k for k, v in NOTE_VALUES.items()}
+CLEF_TRANSPOSE = {
+    "f3": 3,  # not really true?
+    "c3": 3,
+    "c4": 0
+}
+HAS_NOTES = r"[cdefghijklmn]+"
+
 
 files = [
     filename for filename in os.listdir(GABC_DIR)
@@ -31,6 +55,25 @@ for filename in files:
         os.makedirs(mode_dir)
 
     # parse out melody data here
+    # lower case
+    gabc = gabc.lower()
+    # get the clef and lose the text
+    clef, *neumes = re.findall(r"\(.*?\)", gabc)
+    # filter out non-neumes (e.g. breaks)
+    neumes = filter(lambda x: re.search(HAS_NOTES, x), neumes)
+    for neume in neumes:
+        # remove non-note and non-length characters from neumes
+        for char in "()'~v":
+            neume = neume.replace(char, "")
+        # replace quilismae with episemata
+        neume = neume.replace("w", "_")
+        # place bunched episemata adjacent to the notes they affect
+        matches = re.finditer(r"_{2,}", neume)
+        for match in matches:
+            start, end = match.span()
+            n_episemata = end - start
+            end -= n_episemata
+
     melody = gabc
 
     outfilename = re.sub(r"\.gabc$", "", filename)
